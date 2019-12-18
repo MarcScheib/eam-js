ecb7aae482c61f21ed353c7b8577f15e6631a4a0
Marc Scheib <marc.scheib@gmx.de>
2019-12-18 20:56:21 +0100

    feat(projects): add projects heading

--- libs/projects/overview-shell/src/lib/projects-overview-shell.module.ts
+++ libs/projects/overview-shell/src/lib/projects-overview-shell.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { HeadingModule } from '@eam-js/components/heading';
 import { ProjectOverviewComponent } from './project-overview/project-overview.component';
 
 @NgModule({
   imports: [
     CommonModule,
-
     RouterModule.forChild([
       { path: '', pathMatch: 'full', component: ProjectOverviewComponent },
     ]),
+    HeadingModule,
   ],
   declarations: [ProjectOverviewComponent],
 })
