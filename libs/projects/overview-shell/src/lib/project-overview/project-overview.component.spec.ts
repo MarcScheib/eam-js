4182ac2e33d100c80b21954ffd5e64c423861789
Marc Scheib <marc.scheib@gmx.de>
2019-12-23 15:38:22 +0100

    test(projects): fix spec

--- libs/projects/overview-shell/src/lib/project-overview/project-overview.component.spec.ts
+++ libs/projects/overview-shell/src/lib/project-overview/project-overview.component.spec.ts
@@ -1,21 +1,10 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProjectOverviewComponent } from './project-overview.component';
 
 describe('ProjectOverviewComponent', () => {
   let component: ProjectOverviewComponent;
-  let fixture: ComponentFixture<ProjectOverviewComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ProjectOverviewComponent],
-    }).compileComponents();
-  }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ProjectOverviewComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new ProjectOverviewComponent();
   });
 
   it('should create', () => {
