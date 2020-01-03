45eda4548337857512fe03361070c0a7c3ba3115
Marc Scheib <marc.scheib@gmx.de>
2020-01-03 15:14:55 +0100

    test(projects): fix spec

--- libs/projects/ui/src/lib/project-list/project-list.component.spec.ts
+++ libs/projects/ui/src/lib/project-list/project-list.component.spec.ts
@@ -1,24 +1,20 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
 import { ProjectListComponent } from './project-list.component';
+import { ProjectListModule } from './project-list.module';
 
 describe('ProjectListComponent', () => {
-  let component: ProjectListComponent;
-  let fixture: ComponentFixture<ProjectListComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ProjectListComponent],
-    }).compileComponents();
-  }));
+  let spectator: Spectator<ProjectListComponent>;
+  const createComponent = createComponentFactory({
+    component: ProjectListComponent,
+    imports: [ProjectListModule],
+    declareComponent: false,
+  });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ProjectListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    spectator = createComponent();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(spectator.component).toBeTruthy();
   });
 });
