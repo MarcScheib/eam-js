8b746c149d50012f5f1406e1580d5e3e60d7cc71
Marc Scheib <marc.scheib@gmx.de>
2020-02-13 21:00:37 +0100

    feat(projects): add delete to view project

--- libs/projects/feature-shell/src/lib/view-project/view-project.component.ts
+++ libs/projects/feature-shell/src/lib/view-project/view-project.component.ts
@@ -1,7 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import {
+  ConfirmationDialogComponent,
+  ConfirmationDialogData,
+} from '@eam-js/components/confirmation-dialog';
+import { Project } from '@eam-js/projects/api';
 import { ProjectsService } from '@eam-js/projects/data-access';
-import { map, switchMap } from 'rxjs/operators';
+import { map, share, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'view-project',
@@ -12,11 +18,43 @@ import { map, switchMap } from 'rxjs/operators';
 export class ViewProjectComponent {
   project$ = this.route.paramMap.pipe(
     map(params => params.get('id')),
-    switchMap(id => this.projectsService.getByKey(id))
+    switchMap(id => this.projectsService.getByKey(id)),
+    share()
   );
 
   constructor(
     private readonly route: ActivatedRoute,
-    private readonly projectsService: ProjectsService
+    private readonly router: Router,
+    private readonly dialogSvc: MatDialog,
+    private readonly projectsService: ProjectsService,
+    private readonly activatedRoute: ActivatedRoute
   ) {}
+
+  private _goToOverview() {
+    this.router.navigate(['..'], {
+      relativeTo: this.activatedRoute,
+    });
+  }
+
+  delete(project: Project) {
+    this.dialogSvc
+      .open<ConfirmationDialogComponent, ConfirmationDialogData, boolean>(
+        ConfirmationDialogComponent,
+        {
+          data: {
+            title: 'Delete Project',
+            message: `Are you sure you want to delete the project '${project.name}'?`,
+          },
+        }
+      )
+      .afterClosed()
+      .subscribe(confirmed => {
+        if (confirmed) {
+          this.projectsService.delete(project).subscribe(
+            _ => this._goToOverview(),
+            err => console.log(err)
+          );
+        }
+      });
+  }
 }
