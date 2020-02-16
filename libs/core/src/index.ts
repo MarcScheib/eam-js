c6af21aff6ff0780b374a74ebc532fb60d7820dd
Marc Scheib <marc.scheib@gmx.de>
2020-02-16 18:16:39 +0100

    feat(core): add http utilities

--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -1,5 +1,6 @@
 export { CoreModule } from './lib/core.module';
 export { DataRoute, DataRoutes } from './lib/data-route';
+export { RESTClient, AuthHeaderInterceptor, HTTP_CONFIG } from './lib/http';
 export { AppFacade } from './lib/state/app/app.facade';
 export { NavigationFacade } from './lib/state/navigation/navigation.facade';
 export { SettingsFacade } from './lib/state/settings/settings.facade';
