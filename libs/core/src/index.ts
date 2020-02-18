c3fbaa9a94c908f18bcc5d4046113d812bbf1ebe
Marc Scheib <marc.scheib@gmx.de>
2020-02-18 18:44:18 +0100

    feat(core): expose local storage svc

--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -1,6 +1,7 @@
 export { CoreModule } from './lib/core.module';
 export { DataRoute, DataRoutes } from './lib/data-route';
-export { RESTClient, AuthHeaderInterceptor, HTTP_CONFIG } from './lib/http';
+export { AuthHeaderInterceptor, HTTP_CONFIG, RESTClient } from './lib/http';
+export { LocalStorageService } from './lib/local-storage/local-storage.service';
 export { AppFacade } from './lib/state/app/app.facade';
 export { NavigationFacade } from './lib/state/navigation/navigation.facade';
 export { SettingsFacade } from './lib/state/settings/settings.facade';
