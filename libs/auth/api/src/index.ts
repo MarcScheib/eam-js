b30dd16186fb335a2e115b48e176e2da5da98954
Marc Scheib <marc.scheib@gmx.de>
2020-02-19 17:49:31 +0100

    feat(auth): add interface for sign in page data

--- libs/auth/api/src/index.ts
+++ libs/auth/api/src/index.ts
@@ -1,2 +1,3 @@
 export { AuthCredentials } from './lib/auth-credentials';
 export { AuthToken, AuthTokenPayload } from './lib/auth-token';
+export { SignInData } from './lib/sign-in-data';
---
+++ libs/auth/api/src/lib/sign-in-data.ts
@@ -0,0 +1,5 @@
+export interface SignInData {
+  username: string;
+  password: string;
+  keepToken: boolean;
+}
