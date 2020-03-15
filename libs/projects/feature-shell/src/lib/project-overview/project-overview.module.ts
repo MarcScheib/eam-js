676b4d7c939f1788f6948d3e3aa2de68f1de0c80
Marc Scheib <marc.scheib@gmx.de>
2020-03-15 13:55:33 +0100

    refactor(projects): add mat dialog module to overview

--- libs/projects/feature-shell/src/lib/project-overview/project-overview.module.ts
+++ libs/projects/feature-shell/src/lib/project-overview/project-overview.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -17,6 +18,7 @@ import { ProjectOverviewComponent } from './project-overview.component';
     RouterModule,
     MatButtonModule,
     MatCardModule,
+    MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
