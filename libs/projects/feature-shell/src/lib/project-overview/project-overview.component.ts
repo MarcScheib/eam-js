ee3db2f32785d6024477393e33f87f240b6c67d0
Marc Scheib <marc.scheib@gmx.de>
2020-03-15 13:54:49 +0100

    feat(projects): confirm project deletion in overview

--- libs/projects/feature-shell/src/lib/project-overview/project-overview.component.ts
+++ libs/projects/feature-shell/src/lib/project-overview/project-overview.component.ts
@@ -1,4 +1,9 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import {
+  ConfirmationDialogComponent,
+  ConfirmationDialogData,
+} from '@eam-js/components/confirmation-dialog';
 import { Project } from '@eam-js/projects/api';
 import { ProjectsService } from '@eam-js/projects/data-access';
 
@@ -12,7 +17,10 @@ export class ProjectOverviewComponent implements OnInit {
   loading$ = this.projectsService.loading$;
   projects$ = this.projectsService.entities$;
 
-  constructor(private readonly projectsService: ProjectsService) {}
+  constructor(
+    private readonly dialogSvc: MatDialog,
+    private readonly projectsService: ProjectsService
+  ) {}
 
   ngOnInit() {
     this.refresh();
@@ -27,6 +35,24 @@ export class ProjectOverviewComponent implements OnInit {
   }
 
   onDelete(project: Project) {
-    this.projectsService.delete(project.id);
+    this.dialogSvc
+      .open<ConfirmationDialogComponent, ConfirmationDialogData, boolean>(
+        ConfirmationDialogComponent,
+        {
+          data: {
+            title: 'Delete Project',
+            message: `Are you sure you want to delete the project '${project.name}'?`,
+          },
+        }
+      )
+      .afterClosed()
+      .subscribe(confirmed => {
+        if (confirmed) {
+          this.projectsService.delete(project).subscribe(
+            _ => void 0,
+            err => console.log(err)
+          );
+        }
+      });
   }
 }
