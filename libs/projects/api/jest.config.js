0e57ddbc519c3635c0ce03577abfd1d223825dfd
Marc Scheib <marc.scheib@gmx.de>
2021-05-10 17:44:15 +0200

    chore: update to latest nx

--- libs/projects/api/jest.config.js
+++ libs/projects/api/jest.config.js
@@ -5,6 +5,6 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
   coverageDirectory: '../../../coverage/libs/projects/api',
-  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
+  globals: { 'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' } },
   displayName: 'projects-api',
 };
